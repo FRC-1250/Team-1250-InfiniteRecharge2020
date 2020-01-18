1b25f1dac217d8ecb48048d42112e2c962b0db26
Damien - Laptop <dvfullerton@gmail.com>
2020-01-18 15:38:56 -0500

    Constants Updates

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.I2C;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not be used for any other purpose.  All constants should be
@@ -17,6 +19,42 @@ package frc.robot;
  */
 public final class Constants {
 
-    public static final int PANEL_MOTOR = 13;
+    //Robot Devices----------------------------------------
+    //Sub_Drivetrain
+    public static final int DRV_RIGHT_FRONT = 10;
+    public static final int DRV_RIGHT_MID = 11;
+    public static final int DRV_RIGHT_BACK = 12;
+    public static final int DRV_LEFT_FRONT = 13;
+    public static final int DRV_LEFT_MID = 14;
+    public static final int DRV_LEFT_BACK = 15;
+    public static final int DRV_PIGEON = 40;
+
+    //Sub_Shooter
+    public static final int SHOOT_FALCON_0 = 16;
+    public static final int SHOOT_FALCON_1 = 17;
+    public static final int SHOOT_TURRET = 18;
+    public static final int SHOOT_HOOD = 19;
+
+    //Sub_Panel
+    public static final int PANEL_MOTOR = 20;
+    public static final int PANEL_SOL = 0;
+    public static final I2C.Port PANEL_SENSOR_PORT = I2C.Port.kOnboard;
+
+    //Sub_Intake
+    public static final int INT_COL_MOTOR = 21;
+    public static final int INT_HOP_0 = 22;
+    public static final int INT_HOP_1 = 23;
+    public static final int INT_HOP_ELE = 24;
+    public static final int INT_HOP_ELE_SENS = 0;
+
+    //Sub_Climber
+    public static final int CLM_SOL_PTO = 1;
+    public static final int CLM_SOL_EXTEND = 2;
+    public static final int CLM_SPOOL = 25;
+
+    //Constants--------------------------------------------
+    public static final double DRV_GEAR_RATIO = 10;
+    public static final double DRV_WHEEL_SIZE = 6;
+    public static final double DRV_WHEEL_DIAMETER = DRV_WHEEL_SIZE * Math.PI;
 
 }
