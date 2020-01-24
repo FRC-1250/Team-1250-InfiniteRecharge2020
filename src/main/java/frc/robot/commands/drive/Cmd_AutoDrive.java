108f9a39bdc1c2477497936faf28d24f9c858996
Damien - Laptop <dvfullerton@gmail.com>
2020-01-24 12:13:56 -0500

    Migrated Auto Commands

---
+++ src/main/java/frc/robot/commands/drive/Cmd_AutoDrive.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.drive;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Sub_Drivetrain;
+
+public class Cmd_AutoDrive extends CommandBase {
+  private final Sub_Drivetrain s_drivetrain;
+  int distance = 0;
+  double upperSpeed;
+  double lowerSpeed;
+  float sign;
+  public Cmd_AutoDrive(Sub_Drivetrain subsystem, int distance, double upperSpeed, double lowerSpeed) {
+    s_drivetrain = subsystem;
+    addRequirements(subsystem);
+    this.distance = distance;
+    this.upperSpeed = upperSpeed;
+    this.lowerSpeed = lowerSpeed;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    s_drivetrain.resetGyro();
+    s_drivetrain.setSetpointPos(distance);
+    //Note in discord
+    withTimeout(15);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    s_drivetrain.driveToPos(upperSpeed, lowerSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    s_drivetrain.driveStop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    sign = Math.signum(distance);
+        
+    if (sign == 1){
+        return s_drivetrain.isDoneDriving();
+    }
+    if (sign == -1){
+        return s_drivetrain.isDoneDrivingBack();
+    }
+    else{
+        return false;
+    }
+    
+  }
+}
---
+++ src/main/java/frc/robot/commands/drive/Cmd_AutoTurn.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.drive;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Sub_Drivetrain;
+
+public class Cmd_AutoTurn extends CommandBase {
+  private final Sub_Drivetrain s_drivetrain;
+
+  double angle = 0;
+  double upperSpeed = 0;
+  double lowerSpeed = 0;
+  public Cmd_AutoTurn(Sub_Drivetrain subsystem, double angle, double upperSpeed, double lowerSpeed) {
+    s_drivetrain = subsystem;
+    addRequirements(subsystem);
+    this.angle = angle;
+    this.upperSpeed = upperSpeed;
+    this.lowerSpeed = lowerSpeed;
+    s_drivetrain.resetGyro();
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    s_drivetrain.resetGyro();
+    withTimeout(5);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    s_drivetrain.turn(angle, upperSpeed, lowerSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    s_drivetrain.driveStop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return s_drivetrain.isDoneTurning(angle);
+  }
+}
