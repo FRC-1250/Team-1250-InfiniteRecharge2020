b771cb8f9f5b03433e835058bc95d52596ea068d
FRC-1250 <hfarobotics@hfa-dearborn.org>
2020-01-25 19:14:44 -0500

    fixed null pointer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -32,6 +32,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    s_drivetrain = new Sub_Drivetrain();
     
   }
 
