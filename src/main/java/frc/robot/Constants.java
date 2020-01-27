a5da23355542ee20d05d6b10a83efca8301d26e6
Damien - Laptop <dvfullerton@gmail.com>
2020-01-27 17:16:32 -0500

    Updated documentation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -62,10 +62,12 @@ public final class Constants {
     public static final double SHOOT_TURRET_P = 0.6;
     public static final double SHOOT_TURRET_D = 0.0;
 
-    public static final double SHOOT_TURRET_HOME= 2348;
-    public static final double SHOOT_TURRET_LEFT_BOUND= 1120;
-    public static final double SHOOT_TURRET_RIGHT_BOUND= 3560;
+    public static final double SHOOT_TURRET_HOME = 2348;
+    public static final double SHOOT_TURRET_LEFT_BOUND = 1120;
+    public static final double SHOOT_TURRET_RIGHT_BOUND = 3560;
 
+    public static final double DRV_KP_SIMPLE_STRAIT = 0.01;
+    public static final double DRV_KP_SIMPLE = 0.05;
 
 
 }
--- src/main/java/frc/robot/subsystems/Sub_Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Sub_Drivetrain.java
@@ -21,23 +21,29 @@ import frc.robot.Constants;
 
 
 public class Sub_Drivetrain extends SubsystemBase {
-
+  //Drive motors
   CANSparkMax fRightMotor = new CANSparkMax(Constants.DRV_RIGHT_FRONT, MotorType.kBrushless);
   CANSparkMax bRightMotor = new CANSparkMax(Constants.DRV_RIGHT_BACK, MotorType.kBrushless);
   CANSparkMax fLeftMotor = new CANSparkMax(Constants.DRV_LEFT_FRONT, MotorType.kBrushless);
   CANSparkMax bLeftMotor = new CANSparkMax(Constants.DRV_LEFT_BACK, MotorType.kBrushless);
+
+  //Drive groups
   private SpeedControllerGroup gRightSide = new SpeedControllerGroup(fRightMotor, bRightMotor);
   private SpeedControllerGroup gLeftSide = new SpeedControllerGroup(fLeftMotor, bLeftMotor);
+
+  //Diff Drive
   private DifferentialDrive diffDriveGroup = new DifferentialDrive(gLeftSide, gRightSide);
 
+  //Other devices
   PigeonIMU pigeon = new PigeonIMU(Constants.DRV_PIGEON);
-
   Joystick Gamepad = new Joystick(0);
+
+
 
   public static double accumError = 0;
 	//private final double AUTO_TURN_RATE = 0.3;
-	private final double KP_SIMPLE_STRAIT = 0.01;
-	private final double KP_SIMPLE = 0.05;
+	private final double pSimpleStraight = Constants.DRV_KP_SIMPLE_STRAIT;
+	private final double pSimple = Constants.DRV_KP_SIMPLE;
   //private final double KI_SIMPLE = 0.03;
   public double driveSetpoint = 0;
 
@@ -57,42 +63,52 @@ public class Sub_Drivetrain extends SubsystemBase {
     bLeftMotor.setIdleMode(idleMode);
   }
 
-
+  //Actual Drive Method
   public void drive(double left, double right){
     diffDriveGroup.tankDrive(left, right);
   }
 
+  //The drive method that passes the joystick values (Overloaded)
   public void drive(Joystick joy){
     drive(-joy.getY(), -joy.getThrottle());
   }
 
+  //Arcade drive method
   public void driveArcade(Joystick joy){
 		diffDriveGroup.arcadeDrive(-joy.getThrottle(),joy.getZ());
   }
-  
+
+  //Get velocity for any CANSparkMax in this subsys
   public double getVelocity(CANSparkMax motor){
     return motor.getEncoder().getVelocity();
   }
 
+  //Get position for any CANSparkMax in this subsys
   public double getPosition(CANSparkMax motor){
     return motor.getEncoder().getPosition();
   }
 
+  //Resets position of the motors referenced for auton
   public void drivePosReset(){
     fLeftMotor.getEncoder().setPosition(0);
     fRightMotor.getEncoder().setPosition(0);
   }
 
+  //Returns the yaw from the pigeon IMU
   public double getGyroAngle(){
     double[] ypr = new double[3];
     pigeon.getYawPitchRoll(ypr);
     return ypr[0];
   }
 
+  //Resets the agle of the gyro to 0
+  //TODO: Test this
   public void resetGyro(){
     pigeon.addYaw(-getGyroAngle());
   }
 
+  //Configures the maximum amp draw of the drive motors based on temperature of the motors
+  //Linear correct in accordance to some motor law
   public void linearDrivingAmpControl(){
     double leftTemp = fLeftMotor.getMotorTemperature();
     double rightTemp = fRightMotor.getMotorTemperature();
@@ -110,6 +126,7 @@ public class Sub_Drivetrain extends SubsystemBase {
     }
   }
 
+  //Used in linearDrivingAmpControl() to set the current limit of the drive motors
   public void motorCurrentConfig(int limit){
     fRightMotor.setSmartCurrentLimit(limit);
     bRightMotor.setSmartCurrentLimit(limit);
@@ -117,10 +134,12 @@ public class Sub_Drivetrain extends SubsystemBase {
     bLeftMotor.setSmartCurrentLimit(limit);
   }
 
+  //Configures the setpoint var for auton
   public void setSetpointPos(double distance){
     driveSetpoint = (Constants.DRV_TICKS_TO_INCH * distance);
-  }   
+  } 
 
+  //Checks if auto drive command is complete
   public boolean isDoneDriving(){    
     double currVal = this.getPosition(fLeftMotor);
     double distToPos = currVal - driveSetpoint;
@@ -128,6 +147,7 @@ public class Sub_Drivetrain extends SubsystemBase {
     return (distToPos >= 0);
   }
 
+  //Checks if auto drive command is complete for when robot runs backwards
   public boolean isDoneDrivingBack(){   
     double currVal = this.getPosition(fLeftMotor);
     double distToPos = currVal - driveSetpoint;
@@ -135,15 +155,18 @@ public class Sub_Drivetrain extends SubsystemBase {
     return (distToPos <= 0);
   }
 
+  //Checks if auto turn command is complete
   public boolean isDoneTurning(double angle){
     return (Math.abs(angle - this.getGyroAngle()) < 2);
   }
 
+  //Configures a proportional gain for the driving stright during auto
   private double getGainP(double setpoint, double current, double kP){ 	
     double error = setpoint - current;  		
-    return KP_SIMPLE * error;
+    return pSimple * error;
   }
 
+  //Linear ramp control for driving, accepts an upper and lower value
   private double linearRamp(double upperSpeed, double lowerSpeed){
     double diff = (driveSetpoint - (double)Math.abs(getPosition(fLeftMotor)));
     double corrected = .05 * diff;
@@ -154,14 +177,16 @@ public class Sub_Drivetrain extends SubsystemBase {
     return lowerBound;
   }
 
+  //Executes the driving of the robot for auton
   public void driveToPos( double upperSpeed, double lowerSpeed){	
-    double offset = getGainP(0,this.getGyroAngle(),KP_SIMPLE_STRAIT);
+    double offset = getGainP(0,this.getGyroAngle(),pSimpleStraight);
     double sign = Math.signum(driveSetpoint);
     
     diffDriveGroup.arcadeDrive(linearRamp(upperSpeed,lowerSpeed) * sign, 0 + offset);
     
   }
 
+  //Executes the turning of the robot for auton
   public void turn (double angle, double upperSpeed, double lowerSpeed){
     double corrected;
     double rotation = angle - getGyroAngle();
@@ -179,7 +204,8 @@ public class Sub_Drivetrain extends SubsystemBase {
           }
           diffDriveGroup.arcadeDrive(0, corrected);
   }
-
+  
+  //Stops driving---------------------
   public void driveStop(){
     diffDriveGroup.arcadeDrive(0, 0);
   }
@@ -187,6 +213,9 @@ public class Sub_Drivetrain extends SubsystemBase {
   public void pause(){
     drive(0,0);
   }
+  //----------------------------------
+
+
 
   @Override
   public void periodic(){
