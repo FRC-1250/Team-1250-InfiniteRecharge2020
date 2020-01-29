8e31cb5f6deebdaef74b7c09727ca49cef1ef105
eusmanova <45981750+eusmanova@users.noreply.github.com>
2020-01-28 21:05:16 -0500

    bestSpinDirection added

--- src/main/java/frc/robot/commands/panel/Cmd_StopOnColor.java
+++ src/main/java/frc/robot/commands/panel/Cmd_StopOnColor.java
@@ -29,12 +29,12 @@ public class Cmd_StopOnColor extends CommandBase {
   @Override
   public void initialize() {
     s_panel.extendCylinder();
+    s_panel.spinMotor(0.2 * s_panel.bestSpinDirection(color));
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    s_panel.spinMotor(0.2);
   }
 
   // Called once the command ends or is interrupted.
@@ -42,7 +42,6 @@ public class Cmd_StopOnColor extends CommandBase {
   public void end(boolean interrupted) {
     // s_panel.retractCylinders();
     s_panel.stopMotor();
-    
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/subsystems/Sub_Panel.java
+++ src/main/java/frc/robot/subsystems/Sub_Panel.java
@@ -7,7 +7,6 @@
 
 package frc.robot.subsystems;
 
-import java.util.Random;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ColorMatch;
@@ -34,37 +33,13 @@ public class Sub_Panel extends SubsystemBase {
    * Creates a new Sub_Panel.
    */
     
-    CANSparkMax panelMotor = new CANSparkMax(Constants.PANEL_MOTOR, MotorType.kBrushless);
-    I2C.Port i2cPort = Constants.PANEL_SENSOR_PORT;
-    Solenoid panelSol = new Solenoid(Constants.PANEL_SOL);
+  CANSparkMax panelMotor = new CANSparkMax(Constants.PANEL_MOTOR, MotorType.kBrushless);
+  I2C.Port i2cPort = Constants.PANEL_SENSOR_PORT;
+  Solenoid panelSol = new Solenoid(Constants.PANEL_SOL);
 
-    ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-    ColorMatch m_colorMatcher = new ColorMatch();
+  ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  ColorMatch m_colorMatcher = new ColorMatch();
 
-    /*
-    Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-    Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-    Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-    Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
-    */
-
-    // Our colors vvv
-    /*
-    // Without LED (does not work great, sees red as yellow)
-    private final Color kBlueTarget = ColorMatch.makeColor(0.18, 0.47, 0.34);
-    private final Color kGreenTarget = ColorMatch.makeColor(0.23, 0.57, 0.19);
-    private final Color kRedTarget = ColorMatch.makeColor(0.59, 0.08, 0.32);
-    private final Color kYellowTarget = ColorMatch.makeColor(0.40, 0.51, 0.08);
-  */
-  /*
-  
-    // With LED
-    private final Color kBlueTarget = ColorMatch.makeColor(0.17, 0.45, 0.34);
-    private final Color kGreenTarget = ColorMatch.makeColor(0.21, 0.55, 0.24);
-    private final Color kRedTarget = ColorMatch.makeColor(0.42, 0.39, 0.16);
-    private final Color kYellowTarget = ColorMatch.makeColor(0.33, 0.53, 0.13);
-*/
-  // Last try with configured colors
   private final Color kBlueTarget = ColorMatch.makeColor(0.18, 0.46, 0.35);
   private final Color kGreenTarget = ColorMatch.makeColor(0.21, 0.53, 0.25);
   private final Color kRedTarget = ColorMatch.makeColor(0.41, 0.40, 0.17);
@@ -153,21 +128,9 @@ public class Sub_Panel extends SubsystemBase {
     SmartDashboard.putNumber("Confidence", match.confidence);
     SmartDashboard.putString("Detected Color", Character.toString(colorChar));
 
-    /**
-     * In addition to RGB IR values, the color sensor can also return an 
-     * infrared proximity value. The chip contains an IR led which will emit
-     * IR pulses and measure the intensity of the return. When an object is 
-     * close the value of the proximity will be large (max 2047 with default
-     * settings) and will approach zero when the object is far away.
-     * 
-     * Proximity can be used to roughly approximate the distance of an object
-     * or provide a threshold for when an object is close enough to provide
-     * accurate color values.
-     */
     final int proximity = m_colorSensor.getProximity();
 
     SmartDashboard.putNumber("Proximity", proximity);
-
   }
 
   public char getSensorColor() {
@@ -202,13 +165,6 @@ public class Sub_Panel extends SubsystemBase {
     return m_colorSensor.getProximity();
   }
 
-  public char getRandomColor() {
-    final String alphabet = "RGBY";
-    final int N = alphabet.length();
-    Random r = new Random();
-    return alphabet.charAt(r.nextInt(N));
-  }
-
   public boolean stopOnColor(char color) { // parameter for if you want to stop on a specific color
     if (getProximity() > 140) {
       if (color == getSensorColor()) {
@@ -216,6 +172,20 @@ public class Sub_Panel extends SubsystemBase {
       }
     }
     return false;
+  }
+
+  public int bestSpinDirection(char desiredColor) {
+    char[] colors = {'Y', 'R', 'G', 'B'};
+    int colorIndex, desiredColorIndex;
+    if (getProximity() > 140) {
+      colorIndex = findIndex(colors, getSensorColor());
+      desiredColorIndex = findIndex(colors, desiredColor);
+      int indexDiff = colorIndex - desiredColorIndex;
+      if ((indexDiff == -3) || (indexDiff == 1)) {
+        return -1; // spin CCW
+      }
+    }
+    return 1; // spin CW
   }
 
   public void configureButtons() {
